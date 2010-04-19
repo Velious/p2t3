d06baedb733b3622ce29b70c2b80fbf843a68b9b
jones <jones.owen.morgan@gmail.com>
2010-04-18 23:23:10 -0400

    Changed the style sheet some and the page a little. Added a picture for the title and made it the "home" link. Made the site look wikipedia-esque.

--- header.php
+++ header.php
@@ -1,9 +1,7 @@
 <div id="header" style="margin:10px 10px 0px 10px;">
 
-<center><h1 style="font-size:150%">WikiBlog</h1></center></div>
-<div id="nav"><center><p><i>The blog anyone can edit.</i></p></center></div>
+<a href='.'><center><img src="wikiblog.png"/></center></div></a>
+<div id="nav"><center><p><i>The free blog that anyone can edit.</i></p></center></div>
 	<div id="Navigation">
-	<a href=".">Home</a>
-	|
 	<a href="add.php">Add Entry</a>
 	</div>
